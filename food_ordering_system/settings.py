2a14b024dd131e459748e5a5657dcc06bb617372
AbuuIbtisaam <mansoormustapha22@gmail.com>
2024-03-09 19:06:36 +0530

    Add post method for logout instead of href

--- food_ordering_system/settings.py
+++ food_ordering_system/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"  # new
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "home"
 AUTH_USER_MODEL = "accounts.CustomUser"
 
